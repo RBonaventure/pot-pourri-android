057633a88896d42cf41dc7ac23d73c396426b859
Raphaël Bonaventure <raph.bonaventure@gmail.com>
2017-12-10 13:49:46 +0100

    Firestore paths.

---
+++ app/src/main/java/com/rbonaventure/potpourri/utils/FirestoreCollections.java
@@ -0,0 +1,10 @@
+package com.rbonaventure.potpourri.utils;
+
+/**
+ * Created by rbonaventure on 10/12/2017.
+ */
+
+public class FirestoreCollections {
+    public static final String RESOURCES = "resources";
+    public static final String LOCATIONS = "locations";
+}
