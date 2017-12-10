bfd48898c31cf09aad014a4dd763f970018fed6a
Raphaël Bonaventure <raph.bonaventure@gmail.com>
2017-12-10 13:51:34 +0100

    Optimize imports.

--- app/src/main/java/com/rbonaventure/potpourri/ui/CSRvsSSR.java
+++ app/src/main/java/com/rbonaventure/potpourri/ui/CSRvsSSR.java
@@ -1,20 +1,8 @@
 package com.rbonaventure.potpourri.ui;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
-
-import com.google.firebase.perf.FirebasePerformance;
-import com.google.firebase.perf.metrics.Trace;
-import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
-import com.rbonaventure.potpourri.App;
-import com.rbonaventure.potpourri.BuildConfig;
-import com.rbonaventure.potpourri.utils.RemoteConfig;
-import com.rbonaventure.potpourri.utils.Traces;
 
 /**
  * Created by rbonaventure on 10/11/2017.
